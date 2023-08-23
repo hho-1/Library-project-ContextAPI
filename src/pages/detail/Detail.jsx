6ad6ecbdeba951436c4da52b7996bb752e1a78b5
Habip Hakkioglu <h.hakkioglu@icloud.com>
2023-08-23 17:01:56 +0200

    unnecessary imports deleted

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,7 +1,7 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+
+import React from "react";
 import { useLocation, useParams } from "react-router-dom";
-import { useBooksContext } from "../../context/BooksContext";
+
 import {
   Description,
   DetailContainer,
